f89e1de89ff4ea5d8d4d4a1610cb5e984df2c7b5
KochetovaKseniya <kseniyakochetova01@gmail.com>
2024-01-03 15:54:45 +0200

    Update our-team-elementor-addon.php

--- our-team-elementor-addon/our-team-elementor-addon.php
+++ our-team-elementor-addon/our-team-elementor-addon.php
@@ -1,20 +1,27 @@
 <?php 
 /*
     Plugin Name: Our Team Elementor Addon
-    Version: 1.0
-    Description: Add addon Our Team for Elementor.
+    Version:     1.0.0
+    Description: Add addon Our Team for Elementor
     Author: KochetovaKseniya
 */
 
-
-function enqueue_our_team_elementor_styles() {
-    wp_enqueue_style('our-team-styles', plugin_dir_url(__FILE__) . 'css/our-team-styles.css');
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
-add_action('wp_enqueue_scripts', 'enqueue_our_team_elementor_styles');
 
-function register_our_team_elementor_widget() {
-    \Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \YourNamespace\Widgets\Our_Team_Elementor_Widget());
+function our_team_enqueue_styles( $widgets_manager ) {
+
+	require_once( __DIR__ . '/widgets/our-team-elementor-widget.php' );
+
+	$widgets_manager->register( new \Our_Team_Elementor_Widget() );
+
 }
-add_action('elementor/widgets/widgets_registered', 'register_our_team_elementor_widget');
+add_action( 'elementor/widgets/register', 'our_team_enqueue_styles' );
+
+// function our_team_enqueue_styles() {
+//     wp_enqueue_style('our-team-styles', plugin_dir_url(__FILE__) . 'our-team-styles.css');
+// }
+// add_action('wp_enqueue_scripts', 'our_team_enqueue_styles');
 
 ?>
