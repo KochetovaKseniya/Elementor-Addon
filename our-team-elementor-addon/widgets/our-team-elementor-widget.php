bf9c80565e75c3e7a5f01500280f3c85ca23e50d
KochetovaKseniya <kseniyakochetova01@gmail.com>
2024-01-03 15:54:28 +0200

    Update our-team-elementor-widget.php

--- our-team-elementor-addon/widgets/our-team-elementor-widget.php
+++ our-team-elementor-addon/widgets/our-team-elementor-widget.php
@@ -1,13 +1,35 @@
 <?php 
-
-namespace YourNamespace\Widgets;
-
-use Elementor\Widget_Base;
+use \Elementor\Widget_Base;
 
 class Our_Team_Elementor_Widget extends Widget_Base {
 
-    // Start
+	public function get_name() {
+		return 'hello_world_widget_1';
+	}
 
+	public function get_title() {
+		return esc_html__( 'Our team', 'elementor-addon' );
+	}
+
+	public function get_icon() {
+		return 'eicon-code';
+	}
+
+	public function get_categories() {
+		return [ 'basic' ];
+	}
+
+	public function get_keywords() {
+		return [ 'hello', 'world' ];
+	}
+
+	protected function render() {
+		?>
+
+		<p> Hello World </p>
+
+		<?php
+	}
 }
 
 
